1d7c322413b4f4e20c1bc7a6a5772963088af887
jibrna <sitisopiyah160609@gmail.com>
2025-09-23 09:28:26 +0700

    list_spasi

--- lib/list_spasi.dart
+++ lib/list_spasi.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class SpacedList extends StatelessWidget {
+  const SpacedList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const int itemCount = 4;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Spaced List'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          // LayoutBuilder memberikan informasi tinggi maximum parent
+          return SingleChildScrollView(
+            // Agar list bisa di scroll jika tinggi layar terbatas
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: List.generate(
+                  itemCount,
+                  (index) => Card(
+                    margin: const EdgeInsets.all(8),
+                    color: Colors.blue[100 * (index + 2)],
+                    child: SizedBox(
+                      height: 100,
+                      child: Center(child: Text('Item $index',
+                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)))
+                    )
+                ),
+              ))
+            ),
+          );
+        }
+      )
+    );
   }
 }
